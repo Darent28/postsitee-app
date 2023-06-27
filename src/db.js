f3500f1db570f75fb11811cf676349a1f4497729
David Elizondo <davidantonioelizondoramos@gmail.com>
2023-06-27 02:35:53 -0600

    bd update

--- src/db.js
+++ src/db.js
@@ -1,9 +1,17 @@
 import { createPool } from 'mysql2/promise';
 
+// export const pool = createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '1234',
+//     port: 3306,
+//     database: 'postsite'
+// })
+
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
+    host: 'blciovhybuctaljudum4-mysql.services.clever-cloud.com',
+    user: 'u0fg5grryv2iefp0',
+    password: 'OWALdExK0yX80m7xfxhz',
     port: 3306,
-    database: 'postsite'
+    database: 'blciovhybuctaljudum4'
 })
